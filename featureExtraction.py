43a425da5840bac4a544e941afa80cee04fc4a1b
medhini <medhini95@gmail.com>
2016-02-20 10:52:41 +0530

    Fixed ssim

--- featureExtraction.py
+++ featureExtraction.py
@@ -2,6 +2,8 @@ import cv2
 import numpy 
 from skimage.measure import structural_similarity as ssim
 import matplotlib.pyplot as plt
+import os, os.path
+from skimage import color
 
 def localDescriptors(frames):
 
@@ -14,7 +16,7 @@ def localDescriptors(frames):
 		m = m / 10
 		n = n / 8
 
-		descriptors = numpy.zeros((m * n, 13))
+		descriptors = numpy.zeros((m * n, 13)) 
 		patchNumber = 0
 
 		#Spatial Descriptor
@@ -28,7 +30,7 @@ def localDescriptors(frames):
 
 				spatialNeighbours = []
 				
-				#Clock - wise spatialNeighbours starting from left of current patch
+				#AntiClock - wise spatialNeighbours starting from left of current patch
 
 				spatialNeighbours.append(frame[ i - 10 : i, j : j + 8])
 				spatialNeighbours.append(frame[ i - 10 : i, j + 8 : j + 16])
@@ -42,44 +44,61 @@ def localDescriptors(frames):
 				d = 0 #Descriptor number
 
 				for neighbour in spatialNeighbours:
-					descriptors[patchNumber][d] = ssim(neighbour, patch)
+					if(patch.shape == neighbour.shape):
+						patch = color.rgb2gray(patch)
+						neighbour = color.rgb2gray(neighbour)
+						# print patch.shape, neighbour.shape
+						descriptors[patchNumber][d] = ssim(neighbour, patch)
+					else :
+						descriptors[patchNumber][d] = 0	
 					d += 1
 
-				#Temporal Descriptor
+				# Temporal Descriptor
 				for k in range(frameCount + 1, min(frameCount + 6, len(frames))):
 					nextPatch = frames[k][ i : i + 10, j : j + 8]	
-					descriptors[patchNumber][d]	= ssim(nextPatch, patch)
+					if(nextPatch.shape == patch.shape):
+						patch = color.rgb2gray(patch)
+						nextPatch = color.rgb2gray(nextPatch)
+						descriptors[patchNumber][d]	= ssim(nextPatch, patch)
+					else :
+						descriptors[patchNumber][d] = 0
 					d += 1
 
 				patchNumber += 1
-
+				# print patchNumber
+		print frameCount
 	return descriptors
 
 def globalDescriptors(frames):
 	return 
 
-def readFrames(fileName):
+def readFrames(directory):
 	
 	frames = []
-	numberOfFrames = 180
+	fileNames = [os.path.join(directory,name) for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))]
+	fileNames.sort()
+
 	i = 0
 
-	while i < numberOfFrames:
-		frames.append(cv2.imread(fileName))
+	while i < len(fileNames):
+		frames.append(cv2.imread(fileNames[i]))
 		i += 1
 
+	print frames
 	localD = localDescriptors(frames)
 
 	return localD
 
 if __name__ == "__main__":
 
-	x = 'x.jpg'
-	d = readFrames(x)
+	DIR = 'UCSD_Anomaly_Dataset.v1p2/UCSDped2/Train/Train001'
+	d = readFrames(DIR)
 
-	for x in xrange(len(d)):
-		for y in xrange(len(d[0])):
-			print d[x][y],
+	# for x in xrange(len(d)):
+	# 	for y in xrange(len(d[0])):
+	# 		print d[x][y],
+
+
 
 					
 
