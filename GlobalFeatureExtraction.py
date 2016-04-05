3e01bd6e504a86476de920b41e9c4a76a013e860
medhini <medhini95@gmail.com>
2016-04-05 19:44:33 +0530

    Added mahalanobis distance classifier

--- GlobalFeatureExtraction.py
+++ GlobalFeatureExtraction.py
@@ -9,20 +9,21 @@ import scipy.io
 import scipy.optimize
 import time
 from operator import add, sub
+from time import gmtime, strftime
 # from covariance.py import * 
 
 def sigmoid(x):
-	return 1/(1 + np.exp(-x))
+	return (1 / (1 + np.exp(-x)))
 
 def KLDivergence(rho, rhoCap):
-	return np.sum(rho*np.log(rho/rhoCap) + (1 - rho)/np.log((1 - rho)/ (1 - rhoCap)))
+	# print rhoCap
+	return np.sum(rho*np.log(rho/rhoCap) + (1 - rho)*np.log((1 - rho)/ (1 - rhoCap)))
 
 #Covariance
 def getCovarianceandMean(samples):
 
 	height, width = samples.shape
 
-	print height
 	#Calculate the mean of each dimension
 	i = 0
 	j = 0
@@ -37,7 +38,6 @@ def getCovarianceandMean(samples):
 			j += 1	
 		means[i] = sums[i]/height
 		i += 1
-
 
 	#Calculate the variance matrix
 	variance = np.zeros(samples.shape, float)
@@ -56,7 +56,6 @@ def getCovarianceandMean(samples):
 	#calculate the deviation score
 	varianceT = variance.transpose()
 
-	print width
 	deviation = np.zeros((width, width), float)
 
 	deviation = np.dot(varianceT, variance)
@@ -71,7 +70,7 @@ def getCovarianceandMean(samples):
 			j += 1
 		i += 1
 
-	return deviation, means
+	return deviation, means.transpose()
 
 #Global features classifier
 
@@ -79,23 +78,32 @@ def globalClassifier(globalD):
 	
 	covMat, mean = getCovarianceandMean(globalD)
 	covMatInv = np.linalg.inv(covMat)
+	
+	maxThreshold = 0
+	for x in globalD:
+		threshold = np.dot(np.dot((x - mean), covMatInv),(x - mean).transpose())	
+		if threshold > maxThreshold:
+			maxThreshold = threshold
 
-	
-	threshold = np.amax(np.multiply(np.multiply((globalD - mean).transpose(), covMatInv),(globalD - mean)))
-	
-	print threshold
-	return
+		# print np.dot((x - mean), covMatInv)
+	return maxThreshold, mean, covMatInv
 
 
 #Autoencoder
-def globalDescriptors(frames):
-	
+def globalDescriptors(frames, video_name):
+
+	MeanCovarianceFileName = "MeanCovariance/n_MeanCovarianceFile" + video_name + ".npy"
+	WeightsFileName = "Weights/n_WeightsFile" + video_name + ".npy"
+
+	thresholdFileName = "Threshold/n_thresholdFile" + video_name 
+	thresholdFile = open(thresholdFileName, "wb")
+
 	inputLayerSize  = 500    # side length of sampled image patches
 	hiddenLayerSize = 100    # side length of representative image patches
-	rho 						= 0.05   # desired average activation of hidden units
+	rho 			= 0.05   # desired average activation of hidden units
 	lamda          	= 0.0001 # weight decay parameter
 	beta           	= 3      # weight of sparsity penalty term
-	max_iterations 	= 400    # number of optimization iterations
+	max_iterations 	= 1      # number of optimization iterations
 		
 	# Initialize Neural Network weights randomly
 
@@ -107,16 +115,17 @@ def globalDescriptors(frames):
 	
 	# W1, W2 values are chosen in the range [-r, r] 
 
-	# r = np.sqrt(1) / np.sqrt(inputLayerSize + hiddenLayerSize + 1)
+	r = np.sqrt(2.0)/np.sqrt(inputLayerSize + hiddenLayerSize)
 
-	r = 90
 	rand = np.random.RandomState(int(time.time()))
 
-	# W1 = np.asarray(rand.uniform(low = -1/r, high = r, size = (hiddenLayerSize, inputLayerSize)))
-	# W2 = np.asarray(rand.uniform(low = -1/r, high = r, size = (inputLayerSize, hiddenLayerSize)))
+	# W1 = np.asarray(rand.uniform(low = 1, high = 1, size = (hiddenLayerSize, inputLayerSize)))
+	# W2 = np.asarray(rand.uniform(low = 1, high = 1, size = (inputLayerSize, hiddenLayerSize)))
 
-	W1 = np.asarray(1, size = (hiddenLayerSize, inputLayerSize)))
-	W2 = np.asarray(1, size = (inputLayerSize, hiddenLayerSize)))
+	# W1 = np.random.randn(inputLayerSize * hiddenLayerSize) * np.sqrt(2.0)/np.sqrt(inputLayerSize + hiddenLayerSize)
+	# W2 = np.random.randn(inputLayerSize * hiddenLayerSize) * np.sqrt(2.0)/np.sqrt(inputLayerSize + hiddenLayerSize)
+	W1 = np.asarray(rand.uniform(low = -r, high = r, size = (hiddenLayerSize, inputLayerSize)))
+	W2 = np.asarray(rand.uniform(low = -r, high = r, size = (inputLayerSize, hiddenLayerSize)))
 		
 	# Bias values are initialized to zero 
 		
@@ -124,6 +133,7 @@ def globalDescriptors(frames):
 	b2 = np.zeros((inputLayerSize, 1))
 
 	theta = np.concatenate((W1.flatten(), W2.flatten(), b1.flatten(), b2.flatten()))
+	
 	# inputNodes - Each node is intensity of a patch. (10*10*5 values) 
 
 	numberOfFrames = len(frames)
@@ -142,10 +152,14 @@ def globalDescriptors(frames):
 							node.append(intensity)
 				inputNodes.append(node)
 
+	# print inputNodes
 	inputNodes = np.asarray(inputNodes, dtype = np.float32)  #Convert to np array
 	inputNodes = np.transpose(inputNodes)
 	inputNodes = (inputNodes - inputNodes.mean(axis=0)) / inputNodes.std(axis=0) #Normalization
 
+
+	oldCost = 0.0
+	cost = 0.0
 	for iter in xrange(max_iterations):
 
 		W1 = theta[limit0 : limit1].reshape(hiddenLayerSize, inputLayerSize)
@@ -153,23 +167,23 @@ def globalDescriptors(frames):
 		b1 = theta[limit2 : limit3].reshape(hiddenLayerSize, 1)
 		b2 = theta[limit3 : limit4].reshape(inputLayerSize, 1)
 
-		hl = np.dot(W1, inputNodes) + b1
-		
-		hiddenLayer = sigmoid(np.dot(W1, inputNodes) + b1)
+		hiddenLayer = np.dot(W1, inputNodes) + b1
+		hiddenLayer = (hiddenLayer - hiddenLayer.mean(axis=0)) / hiddenLayer.std(axis=0)
+		# hiddenLayer = hiddenLayer/sum(hiddenLayer)
+		hiddenLayer = sigmoid(hiddenLayer)
 		outputLayer = sigmoid(np.dot(W2, hiddenLayer) + b2)
-
-		# for i in range(len(hiddenLayer)):
-		# 	if np.all(hiddenLayer[i] == 0):
-		# 		hiddenLayer[i] += 0.0001
-
-		rhoCap = np.sum(hiddenLayer, axis = 1)/numberOfFrames
+	
+		rhoCap = np.sum(hiddenLayer, axis = 1)/inputNodes.shape[1]
 
 		# print len(outputLayer), len(inputNodes)
 		diff = outputLayer - inputNodes
 
 		# print np.dot(W2, hiddenLayer) + b2
 
-		sumOfSquaresError = 0.5 * np.sum(np.multiply(diff, diff)) / numberOfFrames
+		# if np.all(rhoCap) == 0.0:
+		# 	rhoCap += 0.00001
+
+		sumOfSquaresError = 0.5 * np.sum(np.multiply(diff, diff)) / inputNodes.shape[1]
 		weightDecay       = 0.5 * lamda * (np.sum(np.multiply(W1, W1)) + np.sum(np.multiply(W2, W2)))
 		sparsityPenalty   = beta * KLDivergence(rho, rhoCap)
 		cost              = sumOfSquaresError + weightDecay + sparsityPenalty
@@ -177,61 +191,83 @@ def globalDescriptors(frames):
 		KLDivGrad = beta * (-(rho / rhoCap) + ((1 - rho) / (1 - rhoCap)))
 
 		delOut = np.multiply(diff, np.multiply(outputLayer, 1 - outputLayer))
-		delHid = np.multiply(np.dot(np.transpose(W2), delOut) + np.transpose(np.matrix(KLDivGrad)), np.multiply(hiddenLayer, 1 - hiddenLayer)) 
-		
+		delHid = np.multiply(np.dot(np.transpose(W2), delOut)  + np.transpose(np.matrix(KLDivGrad)), np.multiply(hiddenLayer, 1 - hiddenLayer))
+		delBias = np.multiply(np.dot(np.transpose(W2), delOut), np.multiply(hiddenLayer, 1 - hiddenLayer))
 		# print outputLayer
 		 # + np.transpose(np.matrix(KLDivGrad))
+		
 		#Compute the gradient values by averaging partial derivatives
 			
 		W1Grad = np.dot(delHid, np.transpose(inputNodes))
 		W2Grad = np.dot(delOut, np.transpose(hiddenLayer))
-		b1Grad = np.sum(delHid, axis = 1)
+		b1Grad = np.sum(delBias, axis = 1)
 		b2Grad = np.sum(delOut, axis = 1)
 		
 		#Partial derivatives are averaged over all training examples
 
-		W1Grad = W1Grad / numberOfFrames + lamda * W1
-		W2Grad = W2Grad / numberOfFrames + lamda * W2
-		b1Grad = b1Grad / numberOfFrames
-		b2Grad = b2Grad / numberOfFrames	
+		W1Grad = W1Grad / inputNodes.shape[1] + lamda * W1
+		W2Grad = W2Grad / inputNodes.shape[1] + lamda * W2
+		b1Grad = b1Grad / inputNodes.shape[1]
+		b2Grad = b2Grad / inputNodes.shape[1]	
 
-		W1 = np.array(W1Grad)
-		W2 = np.array(W2Grad)
-		b1 = np.array(b1Grad)
-		b2 = np.array(b2Grad)
+		W1Grad = np.array(W1Grad)
+		W2Grad = np.array(W2Grad)
+		b1Grad = np.array(b1Grad)
+		b2Grad = np.array(b2Grad)
 
-		theta = np.concatenate((W1.flatten(), W2.flatten(), b1.flatten(), b2.flatten()))
-				
-		print sumOfSquaresError	
+		theta = np.concatenate((W1Grad.flatten(), W2Grad.flatten(), b1Grad.flatten(), b2Grad.flatten()))
+			
+		if ((cost - oldCost)*(cost - oldCost) < 0.05):
+			break	
+	print cost
 
-	# W1 = theta[limit0 : limit1].reshape(hiddenLayerSize, inputLayerSize)
-	# globalD = np.dot(W1, inputNodes).transpose()
+	W1 = theta[limit0 : limit1].reshape(hiddenLayerSize, inputLayerSize)
+	np.save(WeightsFileName, W1)
 
-	# globalClassifier(globalD)
+	globalD = np.dot(W1, inputNodes).transpose()
+
+	threshold, mean, covMatInv = globalClassifier(globalD)
 	
-	# np.save('finalWeights', W1)
-	# np.savetxt('globalDescriptors.txt', globalD)
+	thresholdFile.write("%f" % threshold)
+	np.savez(MeanCovarianceFileName, mean = mean, covMatInv = covMatInv)
 
+	thresholdFile.close()
 	return 
 
 def readFrames(directory):
 	
-	frames = []
-	fileNames = [os.path.join(directory,name) for name in os.listdir(directory) if os.path.isfile(os.path.join(directory, name))]
-	fileNames.sort()
+	print "Feature extraction Started"
+	print "Time : "
+	print strftime("%Y-%m-%d %H:%M:%S", gmtime())
+	print
+	
+	videos = []
+	
+	videos = [name for name in os.listdir(directory) if os.path.isdir(os.path.join(directory,name)) and '_gt' not in name]
+	videos.sort()
+	
+	for video in videos:
+		frames = []
+		
+		fileNames = [os.path.join(directory,video,name) for name in os.listdir(os.path.join(directory,video)) if os.path.isfile(os.path.join(directory,video,name)) and name.endswith('.tif')]
+		fileNames.sort()
 
-	i = 0
+		i = 0
 
-	while i < len(fileNames):
-		frames.append(cv2.imread(fileNames[i], cv2.IMREAD_GRAYSCALE))
-		i += 1
-
-	globalDescriptors(frames)
+		while i < len(fileNames):
+			frames.append(cv2.imread(fileNames[i], cv2.IMREAD_GRAYSCALE))
+			i += 1
+		
+		globalDescriptors(frames, video)
+	
+	print "Feature extraction complete"
+	print "Time : "
+	print strftime("%Y-%m-%d %H:%M:%S", gmtime())
 	return 
 
 if __name__ == "__main__":
 
-	DIR = 'UCSD_Anomaly_Dataset.v1p2/UCSDped2/Train/Train001'
+	DIR = 'UCSD_Anomaly_Dataset.v1p2/UCSDped2/Train'
 	readFrames(DIR)
 
 
